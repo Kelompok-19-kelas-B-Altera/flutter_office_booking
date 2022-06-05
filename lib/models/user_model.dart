6e38ab00ca10b7863789fbaaa5c2045336c31847
Reinvy <satriaoner@gmail.com>
2022-06-05 17:26:30 +0700

    Add Static Function for Decode JWT

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class UserModel {
   final int uID;
   final String username;
@@ -16,4 +18,23 @@ class UserModel {
     required this.phone,
     required this.role,
   });
+
+  static tokenDecode(String rawToken) {
+    final token = rawToken.split('.');
+    String partToken = token[1].replaceAll('-', '+').replaceAll('_', '/');
+    switch (partToken.length % 4) {
+      case 0:
+        break;
+      case 2:
+        partToken += '==';
+        break;
+      case 3:
+        partToken += '=';
+        break;
+      default:
+        throw Exception('Illegal base64url string!"');
+    }
+    var data = jsonDecode(utf8.decode(base64Url.decode(partToken)));
+    return data;
+  }
 }
