ffca18ab98da3a98be002b4c6ff74e4b0b5909e6
Reinvy <satriaoner@gmail.com>
2022-06-07 21:32:33 +0700

    Add appbar

--- lib/views/screens/sign_up_screen.dart
+++ lib/views/screens/sign_up_screen.dart
@@ -20,169 +20,168 @@ class SignUpScreen extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: IconButton(
+              splashRadius: 20,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+                size: 25,
+              ),
+            ),
+          ),
+          title: const Text(
+            'Daftar',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.white,
+        ),
         body: SingleChildScrollView(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                    splashRadius: 20,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.black,
-                      size: 25,
-                    ),
-                  ),
-                  const Expanded(flex: 2, child: SizedBox()),
-                  const Text(
-                    'Daftar',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  const Expanded(flex: 3, child: SizedBox())
-                ],
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    const Text(
-                      'Email',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    MyTextField(
-                      myController: emailController,
-                      myHintText: 'Masukan Email',
-                      myValidator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Silahkan masukan email';
-                        }
-                        if (!value.contains(emailValidator)) {
-                          return 'Silahkan masukan email yang valid';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const Text(
-                      'Nama',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    MyTextField(
-                      myController: usernameController,
-                      myHintText: 'Masukan Nama',
-                      myValidator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Silahkan masukan nama';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const Text(
-                      'Password',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    MyPasswordField2(
-                      myController: passwordController,
-                      myHintText: 'Masukan Password',
-                      myObsecureText: true,
-                      myValidator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Silahkan masukan password';
-                        }
-                        if (value.length < 8) {
-                          return 'Password terlalu pendek';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    const Text(
-                      'Ulangi Password',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    MyPasswordField2(
-                      myController: passwordController2,
-                      myHintText: 'Masukan Password',
-                      myObsecureText: true,
-                      myValidator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Silahkan masukan password';
-                        }
-                        if (value.length < 8) {
-                          return 'Password terlalu pendek';
-                        }
-                        if (value.length < 8) {
-                          return 'Password terlalu pendek';
-                        }
-                        if (value != passwordController.text) {
-                          return 'Password tidak sama';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(double.infinity, 40),
-                      ),
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          bool hasLogin = await authViewModel.signUp(
-                            email: emailController.text,
-                            username: usernameController.text,
-                            password: passwordController.text,
-                          );
-
-                          if (hasLogin == true) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Berhasil Mendaftar'),
-                              ),
-                            );
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Email Sudah terdaftar'),
-                              ),
-                            );
-                          }
-                        }
-                      },
-                      child: const Text('Daftar'),
-                    ),
-                  ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Email',
+                  style: TextStyle(fontSize: 20),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 10,
+                ),
+                MyTextField(
+                  myController: emailController,
+                  myHintText: 'Masukan Email',
+                  myValidator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Silahkan masukan email';
+                    }
+                    if (!value.contains(emailValidator)) {
+                      return 'Silahkan masukan email yang valid';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Nama',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                MyTextField(
+                  myController: usernameController,
+                  myHintText: 'Masukan Nama',
+                  myValidator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Silahkan masukan nama';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Password',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                MyPasswordField2(
+                  myController: passwordController,
+                  myHintText: 'Masukan Password',
+                  myObsecureText: true,
+                  myValidator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Silahkan masukan password';
+                    }
+                    if (value.length < 8) {
+                      return 'Password terlalu pendek';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'Ulangi Password',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                MyPasswordField2(
+                  myController: passwordController2,
+                  myHintText: 'Masukan Password',
+                  myObsecureText: true,
+                  myValidator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Silahkan masukan password';
+                    }
+                    if (value.length < 8) {
+                      return 'Password terlalu pendek';
+                    }
+                    if (value.length < 8) {
+                      return 'Password terlalu pendek';
+                    }
+                    if (value != passwordController.text) {
+                      return 'Password tidak sama';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 40),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      bool hasLogin = await authViewModel.signUp(
+                        email: emailController.text,
+                        username: usernameController.text,
+                        password: passwordController.text,
+                      );
+
+                      if (hasLogin == true) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Berhasil Mendaftar'),
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Email Sudah terdaftar'),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  child: const Text('Daftar'),
+                ),
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: Padding(
