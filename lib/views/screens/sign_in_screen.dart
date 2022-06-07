530a15d9a336f1419eb640b3f43b3fb74a3eab4c
Reinvy <satriaoner@gmail.com>
2022-06-07 21:32:41 +0700

    Add appbar

--- lib/views/screens/sign_in_screen.dart
+++ lib/views/screens/sign_in_screen.dart
@@ -18,129 +18,127 @@ class SignInScreen extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: IconButton(
+              splashRadius: 20,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+                size: 25,
+              ),
+            ),
+          ),
+          title: const Text(
+            'Masuk',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 30,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.white,
+        ),
         body: SingleChildScrollView(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                    splashRadius: 20,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back_ios,
-                      color: Colors.black,
-                      size: 25,
-                    ),
-                  ),
-                  const Expanded(flex: 2, child: SizedBox()),
-                  const Text(
-                    'Masuk',
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  const Expanded(flex: 3, child: SizedBox())
-                ],
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Email',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    MyTextField(
-                      myController: emailController,
-                      myHintText: 'Masukan Email',
-                      myValidator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Silahkan masukan email';
-                        }
-                        if (!value.contains(emailValidator)) {
-                          return 'Silahkan masukan email yang valid';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Text(
-                      'Password',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    MyPasswordField(
-                      myController: passwordController,
-                      myHintText: 'Masukan Password',
-                      myValidator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Silahkan masukan password';
-                        }
-                        if (value.length < 8) {
-                          return 'Password terlalu pendek';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        // primary: Colors.black,
-                        minimumSize: const Size(double.infinity, 40),
-                      ),
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          bool hasLogin = await authViewModel.signIn(
-                            email: emailController.text,
-                            password: passwordController.text,
-                          );
-
-                          if (hasLogin == true) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                content: Text('Login Berhasil'),
-                              ),
-                            );
-                            Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const HomeScreen()));
-                          } else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                elevation: 0,
-                                backgroundColor:
-                                    Color.fromRGBO(255, 89, 88, 0.2),
-                                content: Text(
-                                  'Masukkan Username dan Password dengan benar.',
-                                  style: TextStyle(color: Colors.red),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                            );
-                          }
-                        }
-                      },
-                      child: const Text('Masuk'),
-                    ),
-                  ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Email',
+                  style: TextStyle(fontSize: 20),
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 10,
+                ),
+                MyTextField(
+                  myController: emailController,
+                  myHintText: 'Masukan Email',
+                  myValidator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Silahkan masukan email';
+                    }
+                    if (!value.contains(emailValidator)) {
+                      return 'Silahkan masukan email yang valid';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  'Password',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                MyPasswordField(
+                  myController: passwordController,
+                  myHintText: 'Masukan Password',
+                  myValidator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Silahkan masukan password';
+                    }
+                    if (value.length < 8) {
+                      return 'Password terlalu pendek';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    // primary: Colors.black,
+                    minimumSize: const Size(double.infinity, 40),
+                  ),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      bool hasLogin = await authViewModel.signIn(
+                        email: emailController.text,
+                        password: passwordController.text,
+                      );
+
+                      if (hasLogin == true) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Login Berhasil'),
+                          ),
+                        );
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomeScreen()));
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            elevation: 0,
+                            backgroundColor: Color.fromRGBO(255, 89, 88, 0.2),
+                            content: Text(
+                              'Masukkan Username dan Password dengan benar.',
+                              style: TextStyle(color: Colors.red),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        );
+                      }
+                    }
+                  },
+                  child: const Text('Masuk'),
+                ),
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: Padding(
