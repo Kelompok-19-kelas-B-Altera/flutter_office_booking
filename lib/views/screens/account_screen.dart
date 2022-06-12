5fb502f523dd0b014ace217ce1336f287ceeed86
Reinvy <satriaoner@gmail.com>
2022-06-12 21:24:42 +0700

    Slicing account screen

---
+++ lib/views/screens/account_screen.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var queryMedia = MediaQuery.of(context);
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/bg.png'), fit: BoxFit.fill),
+        ),
+        child: SafeArea(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                bottom: queryMedia.size.height * 0.13,
+                child: Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(
+                        queryMedia.size.width * 0.9,
+                        queryMedia.size.height * 0.06,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text('Masuk'),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: queryMedia.size.height * 0.05,
+                child: Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.white,
+                      minimumSize: Size(
+                        queryMedia.size.width * 0.9,
+                        queryMedia.size.height * 0.06,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Masuk',
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: queryMedia.size.height * 0.05,
+                child: const Text(
+                  'Akun',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: queryMedia.size.height * 0.12,
+                left: queryMedia.size.width * 0.04,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    SizedBox(
+                      width: 300,
+                      child: Text(
+                        'Temukan pusat kantor terbaik di Jakarta',
+                        style: TextStyle(
+                          fontSize: 32,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 250,
+                      child: Text(
+                        'Cari tempat kantor dimanapun yang kamu inginkan',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
