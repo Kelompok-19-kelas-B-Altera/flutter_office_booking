118830d63f7f61d68e1ec92f55f2fd41a5ea3cc7
Reinvy <satriaoner@gmail.com>
2022-06-15 01:39:30 +0700

    Add building view model

--- lib/view_models/building_view_model.dart
+++ lib/view_models/building_view_model.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_office_booking/models/api/building_api.dart';
 
-class BuildingViewModel with ChangeNotifier {}
+class BuildingViewModel with ChangeNotifier {
+  getAllBuilding(token) {
+    BuildingApi.getAllBuilding();
+  }
+
+  getBuildingById(token, id) {
+    BuildingApi.getBuildingById(id);
+  }
+}
