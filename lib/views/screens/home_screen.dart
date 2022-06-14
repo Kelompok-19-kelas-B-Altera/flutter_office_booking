349498dc4771f9ae39d5643bec37f8f08920fe89
Reinvy <satriaoner@gmail.com>
2022-06-15 01:56:41 +0700

    Change item count

--- lib/views/screens/home_screen.dart
+++ lib/views/screens/home_screen.dart
@@ -73,7 +73,7 @@ class HomeScreen extends StatelessWidget {
                           height: 20,
                         );
                       },
-                      itemCount: 100,
+                      itemCount: 10,
                     ),
                   ),
                 ],
