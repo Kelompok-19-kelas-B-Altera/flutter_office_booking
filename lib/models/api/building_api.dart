f4530ed5cd22e5f8e445531c386c7ca347401209
Reinvy <satriaoner@gmail.com>
2022-06-21 19:46:23 +0700

    Add function for parse response data to list

--- lib/models/api/building_api.dart
+++ lib/models/api/building_api.dart
@@ -1,7 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_office_booking/models/building_model.dart';
 
 class BuildingApi {
-  static getAllBuilding() async {
+  static Future getAllBuilding() async {
     BaseOptions options = BaseOptions(
         receiveDataWhenStatusError: true,
         connectTimeout: 30 * 1000, // 30 seconds
@@ -10,13 +11,28 @@ class BuildingApi {
     var dio = Dio(options);
 
     try {
-      var response = await dio.get('http://108.136.240.248/api/v1/building');
-      print(response.data);
-      return response;
+      final response = await dio.get('http://108.136.240.248/api/v1/building');
+
+      List<BuildingModel> buildings = (response.data['data'] as List)
+          .map((e) => BuildingModel(
+                id: e['id'],
+                buildingName: e['building_name'],
+                totalRoom: e['total_room'],
+                roomSpace: e['room_space'],
+                address: e['address'],
+                imageUrl: e['image_url'],
+                totalView: e['total_view'],
+                complex: ComplexModel(
+                  id: e['complex']['id'],
+                  city: e['complex']['city'],
+                ),
+                nearbyFacilities: e['nearby_facilities'],
+              ))
+          .toList();
+      return buildings;
     } on DioError catch (e) {
       if (e.type == DioErrorType.response) {
         print(e);
-        print('false');
         return null;
       }
     }
