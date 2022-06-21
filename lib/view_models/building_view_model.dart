5fe97fa098cc5129d1970dc791b034d0534c8dce
Reinvy <satriaoner@gmail.com>
2022-06-21 19:47:03 +0700

    Add variable buildingList

--- lib/view_models/building_view_model.dart
+++ lib/view_models/building_view_model.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_office_booking/models/api/building_api.dart';
+import 'package:flutter_office_booking/models/building_model.dart';
 
 class BuildingViewModel with ChangeNotifier {
-  Future getAllBuilding(token) async {
+  List<BuildingModel> buildingList = [];
+
+  Future getAllBuilding() async {
     var kembalian = await BuildingApi.getAllBuilding();
     if (kembalian != null) {
+      buildingList = kembalian;
+      print(buildingList[1].complex.city);
+      notifyListeners();
       return true;
     } else {
+      notifyListeners();
       return false;
     }
   }
