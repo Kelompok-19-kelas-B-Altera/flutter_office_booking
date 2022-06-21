8543d8831286d973e66743662c3e667e52d902f0
Reinvy <satriaoner@gmail.com>
2022-06-21 23:09:34 +0700

    Add method save/load login data in local storage

--- lib/models/storage/local_storage.dart
+++ lib/models/storage/local_storage.dart
@@ -1,1 +1,40 @@
-class LocalStorage {}
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorage {
+  static setLoginData({
+    required String email,
+    required String password,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    final data = {
+      "email": email,
+      "password": password,
+    };
+
+    String stringData = json.encode(data);
+    final encryptData = utf8.encode(stringData);
+    final doubleEncryptData = base64.encode(encryptData);
+
+    if (prefs.containsKey('loginData')) {
+      await prefs.remove('loginData');
+    }
+    await prefs.setString('loginData', doubleEncryptData);
+  }
+
+  static Future getLoginData() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    var doubleEncryptData = prefs.getString('loginData');
+    if (doubleEncryptData != null) {
+      var encryptData = base64.decode(doubleEncryptData);
+      var stringData = utf8.decode(encryptData);
+      var data = json.decode(stringData);
+      return data;
+    } else {
+      return null;
+    }
+  }
+}
