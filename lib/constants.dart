83592ea29a5109b42293ac3f2cadcd0983817638
Reinvy <satriaoner@gmail.com>
2022-07-03 20:50:18 +0700

    Delete comment

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 
-//API Url
+//API Base Url
 String baseUrl = 'http://108.136.240.248';
-// String baseUrl =
-//     'https://novelku-9f1c6-default-rtdb.asia-southeast1.firebasedatabase.app/';
 
 //Color
 Color colorPrimary = const Color.fromARGB(255, 255, 0, 0);
 Color colorSecondary = const Color.fromARGB(255, 85, 255, 0);
 Color colorTertiary = const Color.fromARGB(255, 144, 0, 255);
+Color starColor = const Color.fromARGB(255, 255, 191, 96);
 
 //Application Name
 String appName = 'Officity';
