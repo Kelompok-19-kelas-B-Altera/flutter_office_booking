bb93cf488a476700d6c507c2760564b601d79463
Reinvy <satriaoner@gmail.com>
2022-07-13 17:03:50 +0700

    Add new method

--- lib/view_models/auth_view_model.dart
+++ lib/view_models/auth_view_model.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_office_booking/models/api/user_api.dart';
 import 'package:flutter_office_booking/models/storage/local_storage.dart';
 import 'package:flutter_office_booking/models/user_model.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AuthViewModel with ChangeNotifier {
   UserModel? userData;
@@ -14,6 +15,7 @@ class AuthViewModel with ChangeNotifier {
   }
 
   void _init() async {
+    print('object');
     var data = await LocalStorage.getLoginData();
     if (data != null) {
       await signIn(
@@ -21,6 +23,18 @@ class AuthViewModel with ChangeNotifier {
         password: data['password'],
       );
     }
+  }
+
+  updateData() async {
+    var data = await UserApi.getUserData(userData!.id, _token);
+    userData = UserModel(
+      id: data['id'],
+      email: data['email'],
+      fullName: data['fullname'],
+      role: 'USER',
+      picUrl: data['pic_url'],
+    );
+    notifyListeners();
   }
 
   logOut() async {
@@ -43,15 +57,14 @@ class AuthViewModel with ChangeNotifier {
     if (response != null) {
       _token = response['token'];
       var data = await UserModel.tokenDecode(response['token']);
+      var imgUrl = await UserApi.getUserData(data['id'], _token);
       userData = UserModel(
         id: data['id'],
         email: data['email'],
         fullName: data['fullname'],
         role: data['role'],
+        picUrl: imgUrl['pic_url'],
       );
-
-      print(userData!.id);
-      print(_token);
 
       LocalStorage.setLoginData(
         email: email,
@@ -79,6 +92,23 @@ class AuthViewModel with ChangeNotifier {
       return true;
     } else {
       return false;
+    }
+  }
+
+  addProfileImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        await UserApi.postProfilImage(
+          userData!.id,
+          token,
+          image,
+        );
+      }
+      updateData();
+    } catch (e) {
+      print('object');
+      print(e);
     }
   }
 }
