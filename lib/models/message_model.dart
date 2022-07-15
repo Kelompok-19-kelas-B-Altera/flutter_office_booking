536cbfe041d46b2a8bcee4e26aecb431713451cd
Reinvy <satriaoner@gmail.com>
2022-07-15 21:55:35 +0700

    Add chat room and message model

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,45 +1,82 @@
 class MessageModel {
-  final String text;
-  final DateTime date;
-  final bool isSentByMe;
+  List<GetAllChatByUsersIdAndBuildingId>? getAllChatByUsersIdAndBuildingId;
 
-  MessageModel({
-    required this.text,
-    required this.date,
-    required this.isSentByMe,
-  });
+  MessageModel({this.getAllChatByUsersIdAndBuildingId});
+
+  MessageModel.fromJson(Map<String, dynamic> json) {
+    if (json['getAllChatByUsersIdAndBuildingId'] != null) {
+      getAllChatByUsersIdAndBuildingId = <GetAllChatByUsersIdAndBuildingId>[];
+      json['getAllChatByUsersIdAndBuildingId'].forEach((v) {
+        getAllChatByUsersIdAndBuildingId!
+            .add(GetAllChatByUsersIdAndBuildingId.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (getAllChatByUsersIdAndBuildingId != null) {
+      data['getAllChatByUsersIdAndBuildingId'] =
+          getAllChatByUsersIdAndBuildingId!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
 }
 
-List<MessageModel> message = [
-  MessageModel(text: 'ada', date: DateTime.now(), isSentByMe: false),
-  MessageModel(
-      text: 'text', date: DateTime(2017, 9, 7, 17, 30), isSentByMe: true),
-  MessageModel(
-      text: 'gadwqw wqdqw dqwd wq dqwsasdawd awdawsdawd awdawsdsa',
-      date: DateTime.now(),
-      isSentByMe: true),
-  MessageModel(
-      text: 'trr', date: DateTime(2017, 9, 13, 17, 30), isSentByMe: false),
-  MessageModel(
-      text: 'trr', date: DateTime(2021, 7, 13, 17, 30), isSentByMe: false),
-  MessageModel(
-      text: 'trr', date: DateTime(2021, 9, 13, 17, 30), isSentByMe: false),
-  MessageModel(
-      text: 'trr', date: DateTime(2017, 9, 13, 17, 30), isSentByMe: false),
-  MessageModel(text: 'aawww', date: DateTime.now(), isSentByMe: true),
-  MessageModel(text: 'gad', date: DateTime.now(), isSentByMe: true),
-  MessageModel(
-      text: 'trr dawdawda wda dawdawdawsxwda wdaxawd awxasswd awdawd awdaswa ',
-      date: DateTime.now(),
-      isSentByMe: false),
-  MessageModel(text: 'aawww', date: DateTime.now(), isSentByMe: true),
-  MessageModel(text: 'gad', date: DateTime.now(), isSentByMe: true),
-  MessageModel(text: 'trr', date: DateTime.now(), isSentByMe: false),
-  MessageModel(text: 'aawww', date: DateTime.now(), isSentByMe: true),
-  MessageModel(text: 'gad', date: DateTime.now(), isSentByMe: true),
-  MessageModel(text: 'trr', date: DateTime.now(), isSentByMe: false),
-  MessageModel(text: 'aawww', date: DateTime.now(), isSentByMe: true),
-  MessageModel(text: 'gad', date: DateTime.now(), isSentByMe: true),
-  MessageModel(text: 'trr', date: DateTime.now(), isSentByMe: false),
-  MessageModel(text: 'aawww', date: DateTime.now(), isSentByMe: true),
-];
+class GetAllChatByUsersIdAndBuildingId {
+  String? id;
+  String? message;
+  String? date;
+  Sender? sender;
+  Sender? target;
+
+  GetAllChatByUsersIdAndBuildingId(
+      {this.id, this.message, this.date, this.sender, this.target});
+
+  GetAllChatByUsersIdAndBuildingId.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    message = json['message'];
+    date = json['date'];
+    sender = json['sender'] != null ? Sender.fromJson(json['sender']) : null;
+    target = json['target'] != null ? Sender.fromJson(json['target']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['message'] = message;
+    data['date'] = date;
+    if (sender != null) {
+      data['sender'] = sender!.toJson();
+    }
+    if (target != null) {
+      data['target'] = target!.toJson();
+    }
+    return data;
+  }
+}
+
+class Sender {
+  String? id;
+  String? fullname;
+  String? email;
+  String? image;
+
+  Sender({this.id, this.fullname, this.email, this.image});
+
+  Sender.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    fullname = json['fullname'];
+    email = json['email'];
+    image = json['image'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['fullname'] = fullname;
+    data['email'] = email;
+    data['image'] = image;
+    return data;
+  }
+}
